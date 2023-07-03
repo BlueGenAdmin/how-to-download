3ed00b1e8ea72ad527514cb9455746ba98b6687b
BlueGenAdmin <alex.bluegenesistech@gmail.com>
2023-07-03 18:36:01 +0800

    added language features

---
+++ website/js/language.js
@@ -0,0 +1,167 @@
+
+
+function changeLanguage() {
+    var languageSelect = document.getElementById("languageSelect");
+    var selectedLanguage = languageSelect.value;
+    var whoAreWe = document.getElementById("whoAreWe");
+    var whoAreWe2 = document.getElementById("whoAreWe2");
+    var mtt = document.getElementById("mtt");
+    var mtt2 = document.getElementById("mtt2");
+    var squidTexasHoldem = document.getElementById("squidTexasHoldem")
+    var squidTexasHoldem2 = document.getElementById("squidTexasHoldem2")
+    var wannaChkOut = document.getElementById("wannaChkOut")
+    var wannaChkOut2 = document.getElementById("wannaChkOut2")
+    var wannaChkOut3 = document.getElementById("wannaChkOut3")
+    var wannaChkOut3 = document.getElementById("wannaChkOut3")
+    var wannaChkOut4 = document.getElementById("wannaChkOut4")
+    var wannaChkOut5 = document.getElementById("wannaChkOut5")
+    var wannaChkOut6 = document.getElementById("wannaChkOut6")
+    var wannaChkOut7 = document.getElementById("wannaChkOut7")
+
+    switch (selectedLanguage) {
+        case "chi":
+            whoAreWe.innerText = "我们是谁？";
+            whoAreWe2.innerText = "Ace Advantage 是 Mil Poker 世界的先驱俱乐部之一！ 凭借对游戏的热情和对卓越的承诺，Ace Advantage 已成为创新、战略游戏玩法和无与伦比的游戏体验的代名词。 我们拥有一个由熟练扑克玩家组成的专门社区，为扑克爱好者提供了一个可以磨练技能、竞争和交流的平台。 我们的俱乐部以提供公平和安全的环境而自豪，确保每个球员都有平等的成功机会。 立即加入 Ace Advantage，体验 Mil Poker 的刺激。"
+            mtt.innerText = "MTT 锦标赛";
+            mtt2.innerText = "Ace Advantage 的多桌锦标赛 (MTT) 是对技巧、策略和勇气的终极考验。 来自世界各地的众多参与者，玩家可以展示自己的才华并争夺丰厚的奖池。 我们的 MTT 采用动态结构，随着比赛的进行，盲注和赌注不断增加，可保持较高的兴奋度。 当您浏览多个牌桌时，体验肾上腺素激增的感觉，做出精心策划的动作并击败对手。 加入 Ace Advantage 的 MTT，在最具挑战性和回报最高的 Mil Poker 锦标赛中证明您的技能。"
+            squidTexasHoldem.innerText = "鱿鱼德州扑克";
+            squidTexasHoldem2.innerText = "Squid Poker 是经典德州扑克游戏的惊险变体。 它保留了其前身的核心规则和结构，但引入了一个有趣的变化：鱿鱼存款的概念。 Squid 押金是玩家除了最低买入额之外还必须支付的预定金额。 目标很简单：避免成为最后一个没有赢得鱿鱼的玩家，因为这样做会导致损失整个鱿鱼押金并将其平均分配给其他玩家。 Squid 存款已迅速成为 Squid 德州扑克的一个显着特征，创造了一个激烈且竞争激烈的环境，每个决定都至关重要。 玩家会被激励采取新策略并仔细评估自己的行动，因为一个失误就可能会耗尽他们的押金，并可能让他们口袋空空。"
+            wannaChkOut.innerText = "想看看我们吗？";
+            wannaChkOut2.innerText = "这里在";
+            wannaChkOut3.innerText = "姓名";
+            wannaChkOut4.innerText = "电子邮件";
+            wannaChkOut5.innerText = "你需要什么？";
+            wannaChkOut6.innerText = "给我们留言吧！";
+            wannaChkOut7.innerText = "想知道更多？ 我们会让你知道";
+         
+
+            
+            
+            break;
+        case "en":
+            window.location.reload();
+            break;
+  
+        default:
+            textElement.innerText = "Hello, world!";
+            break;
+    }
+}
+
+
+
+
+function policy(){
+    var languageSelect = document.getElementById("languageSelect");
+    var selectedLanguage = languageSelect.value;
+
+    var policy1 = document.getElementById("policy1")
+    var policy2 = document.getElementById("policy2")
+    var policy3 = document.getElementById("policy3")
+    var policy4 = document.getElementById("policy4")
+    var policy5 = document.getElementById("policy5")
+    var policy6 = document.getElementById("policy6")
+    var policy7 = document.getElementById("policy7")
+    var policy8 = document.getElementById("policy8")
+    var policy9 = document.getElementById("policy9")
+    var policy10 = document.getElementById("policy10")
+    var policy11 = document.getElementById("policy11")
+    var policy12 = document.getElementById("policy12")
+    var policy13 = document.getElementById("policy13")
+    var policy14 = document.getElementById("policy14")
+    var policy15= document.getElementById("policy15")
+    var policy16 = document.getElementById("policy16")
+    var policy17= document.getElementById("policy17")
+    var policy18= document.getElementById("policy18")
+    var policy19= document.getElementById("policy19")
+    var policy20= document.getElementById("policy20")
+    var policy21= document.getElementById("policy21")
+    var policy22= document.getElementById("policy22")
+    var policy23= document.getElementById("policy23")
+    var policy24= document.getElementById("policy24")
+    var policy25= document.getElementById("policy25")
+    var policy26= document.getElementById("policy26")
+    var policy27= document.getElementById("policy27")
+    var policy28= document.getElementById("policy28")
+    var policy29= document.getElementById("policy29")
+    var policy30= document.getElementById("policy30")
+    var policy31= document.getElementById("policy31")
+    var policy32= document.getElementById("policy32")
+    var policy33= document.getElementById("policy33")
+    var policy34= document.getElementById("policy34")
+    var policy35= document.getElementById("policy35")
+    var policy36= document.getElementById("policy36")
+    var policy37= document.getElementById("policy37")
+    var policy38= document.getElementById("policy38")
+    var policy39= document.getElementById("policy39")
+    var policy40= document.getElementById("policy40")
+    var policy41= document.getElementById("policy41")
+    var policy42= document.getElementById("policy42")
+    var policy43= document.getElementById("policy43")
+    var policy44= document.getElementById("policy44")
+    var policy45= document.getElementById("policy45")
+
+
+
+    switch (selectedLanguage) {
+        case "chi":
+        policy1.innerText= "Ace Advantage 隐私政策生效日期：[日期] 本隐私政策描述了 Ace Advantage（“我们”或“我们的”）如何收集、使用、披露和保护其用户（“您”）的个人信息 或“您的”）与您使用 Ace Advantage 网站、移动应用程序和相关服务（统称为“服务”）有关。 通过访问或使用服务，您同意本隐私政策的条款。"
+        policy2.innerText="1. 我们收集的信息"
+        policy3.innerText="1.1 个人信息"
+        policy4.innerText="当您注册帐户、参加游戏或锦标赛、与我们沟通或以其他方式使用我们的服务时，我们可能会收集您直接提供给我们的个人信息。 这些信息可能包括："
+        policy5.innerText="- 您的姓名、用户名和电子邮件地址。"
+        policy6.innerText="- 您的联系信息，例如您的地址和电话号码。"
+        policy7.innerText="- 付款信息，例如信用卡详细信息或其他财务信息。"
+        policy8.innerText="- 您选择向我们提供的任何其他信息。"
+        policy9.innerText="1.2 使用信息"
+        policy10.innerText="当您使用我们的服务时，我们可能会自动收集有关您使用服务的某些信息，包括："
+        policy11.innerText="- 设备信息，例如您的设备类型、操作系统和浏览器类型。"
+        policy12.innerText="- 日志信息，例如您的 IP 地址、访问时间和访问的页面。"
+        policy13.innerText="- 游戏和交易历史记录。"
+        policy14.innerText="- Cookie 和类似技术，用于跟踪您在我们的服务上的活动并个性化您的体验。"
+        policy15.innerText="2. 信息的使用"
+        policy16.innerText="我们将收集的信息用于以下目的："
+        policy17.innerText="- 提供、维护和改进我们的服务并定制您的体验。"
+        policy18.innerText="- 处理和完成交易，包括付款。"
+        policy19.innerText="- 与您沟通、回复您的询问并提供客户支持。"
+        policy20.innerText="- 根据您的喜好向您发送促销和营销信息。"
+        policy21.innerText="- 执行我们的服务条款并保护我们服务的安全性和完整性。"
+        policy22.innerText="- 遵守法律义务并在必要时协助执法机构。"
+        policy23.innerText="3. 信息共享"
+        policy24.innerText="我们可能会在以下情况下共享您的信息："
+        policy25.innerText="- 与我们的附属公司、合作伙伴和服务提供商合作，他们协助我们运营我们的服务并向您提供所需的功能和服务。"
+        policy26.innerText="- 必要时与服务的其他用户共享，以促进游戏和锦标赛的进行。"
+        policy27.innerText="- 出于营销、广告或其他促销目的与第三方共享，但须遵守您的偏好和适用法律。"
+        policy28.innerText="- 响应传票、法院命令或其他法律程序。"
+        policy29.innerText="- 保护我们的权利、财产或安全，或他人的权利、财产或安全。"
+        policy30.innerText="4. 数据安全"
+        policy31.innerText="我们采取合理措施保护您的个人信息的安全，并确保根据本隐私政策安全地处理您的个人信息。 然而，没有一种通过互联网或电子存储进行传输的方法是完全安全的，我们不能保证绝对的安全。"
+        policy32.innerText="5. 你的选择"
+        policy33.innerText="您可以通过[联系信息]联系我们来更新、更正或删除您的帐户信息。 您还可以按照这些电子邮件中提供的说明选择不接收我们发送的促销电子邮件。 请注意，即使您选择不接收促销电子邮件，我们仍可能向您发送交易或管理通信。"
+        policy34.innerText="6. 第三方链接和服务"
+        policy35.innerText="我们的服务可能包含不受我们控制或运营的第三方网站或服务的链接。 本隐私政策不适用于这些第三方网站或服务。 我们不对这些第三方的隐私惯例负责，我们鼓励您在向他们提供任何个人信息之前查看他们的隐私政策。"
+        policy36.innerText="7. 儿童隐私"
+        policy37.innerText="我们的服务不适合 [年龄] 岁以下的个人。 我们不会故意收集儿童的个人信息。 如果我们发现我们在未经父母同意的情况下收集了儿童的个人信息，我们将采取措施删除该信息。"
+        policy38.innerText="8. 本隐私政策的变更"
+        policy39.innerText="我们可能会不时更新本隐私政策"
+        policy40.innerText="时间。 修订后的政策将在发布后立即生效。 我们鼓励您定期查看本隐私政策，以随时了解我们的做法。"
+        policy41.innerText="9. 联系我们"
+        policy42.innerText="如果您对本隐私政策有任何疑问、疑虑或建议，请通过[联系信息]与我们联系。"
+        policy43.innerText="通过使用 Ace Advantage 服务，您承认您已阅读并理解本隐私政策，并同意按照本文所述收集、使用和披露您的个人信息。"
+        policy44.innerText = "我们的政策"
+         policy45.innerText = "我们的政策"
+            
+            break;
+        case "en":
+            window.location.reload();
+            break;
+  
+        default:
+            textElement.innerText = "Hello, world!";
+            break;
+    }
+
+
+
+
+}
